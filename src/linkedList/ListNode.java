31f6ae319ac1e6a3c24688beaaa4f0ad6313cfe4
JR <3142734033@qq.com>
2022-06-19 20:16:22 +0800

    链表的结点

---
+++ src/linkedList/ListNode.java
@@ -0,0 +1,18 @@
+package linkedList;
+
+public class ListNode {
+    public int value;
+
+    public ListNode next;
+
+    public ListNode(){}
+
+    public ListNode(int value) {
+        this.value = value;
+    }
+
+    public ListNode(int value, ListNode next) {
+        this.value = value;
+        this.next = next;
+    }
+}
